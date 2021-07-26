cd790606fd234c9da125b96dccd17ade355d6ee8
holy frege <holy_frege@fastmail.com>
2021-07-26 17:26:43 +0900

    changed home

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "3.34.94.252", user: "deploy", roles: %w{app db web}
+server "13.125.158.130", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
